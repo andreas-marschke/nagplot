ed499c8d6f35104c32a2f6054e0e0e722bb60c0a
Andreas Marschke <andreas.marschke@gmail.com>
2012-01-17 22:37:57 +0100

    add DataSources and Nagios-DataSource implementation

---
+++ lib/Nagplot/DataSource.pm
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+Nagplot::DataSource - The source of all monitorable data
+
+=head1 SYNOPSIS
+
+ use Nagplot::DataSource;
+ my $datasource = Nagplot::DataSource->new( 
+   config => {
+     DataSource => {
+       Nagios => {
+  	 host => '127.0.0.1',
+	 cgi_path => '/nagios/cgi-bin',
+	 nagios_path => '/nagios',
+	 pnp4nagios_path => '/pnp4nagios',
+	 user => 'nagplot',
+	 pass => 'nagplot',
+	 secure => 1
+        }
+      }
+    }
+  );
+
+ my @hosts = $datasource->hosts();
+ my %services;
+
+ foreach (@hosts) {
+   $services{$_} = $datasource->services($_);
+ }
+
+ foreach my $host (keys %services) {
+   foreach my $service (@{$services{$host}}) {
+     print $datasource->query_state($host,$service);
+   }
+ }
+
+=head1 DESCRIPTION
+
+Nagplot Datasource is the root of all data you can pull out of a pluggable backend.
+See Nagplot/DataSource/Backends.pod for more Info
+
+=cut
+
+package Nagplot::DataSource;
+
+our $VERSION = '0.1';
+
+use 5.010_000;
+
+use strict;
+use warnings;
+use Moose;
+use Module::Pluggable search_path => '', require => 1, instantiate => 'new';
+
+has 'config' => ( is => 'rw' , isa => 'Ref', required => 1);
+
+sub hosts {
+  my $self = shift;
+
+  my @hosts;
+  foreach my $plugin ($self->plugins($self->config->{DataSource})) {
+     push (@hosts,$plugin->hosts());
+  }
+  return @hosts;
+}
+
+sub services {
+  my $self = shift;
+  my $host = shift;
+  my @services;
+  foreach my $plugin ($self->plugins($self->config->{DataSource})) {
+    push (@services,$plugin->services());
+  }
+  return @services;
+}
+
+# this is very bad. (performance-wise)
+# later on maybe implement a cache 
+# that holds in which plugin $service
+# and $host are and then just use that
+# plugin to query for it.
+
+sub query_state {
+  my $self = shift;
+  my $host = shift;
+  my $service = shift;
+  my $state;
+  foreach my $plugin ($self->plugins($self->config->{DataSource})) {
+    foreach my $plugin_host ($plugin->hosts) {
+      if ($plugin_host == $host) {
+	foreach my $plugin_service ($plugin->services()) {
+	  if ($plugin_service == $service) {
+	    $state = $plugin->query_state($host, $service);
+	  }
+	}
+      }
+    }
+  }
+  return $state;
+}
+
+
+
+
+no Moose;
+1;
+
+
---
+++ lib/Nagplot/DataSource/Nagios.pm
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+Nagplot::DataSource::Nagios - Nagios Backend for Nagplot::DataSource
+
+=head1 DESCRIPTION
+
+=cut
+
+package Nagplot::DataSource::Nagios;
+
+our $VERSION = '';
+
+use 5.010_000;
+
+use strict;
+use warnings;
+use Moose;
+use Mojo::UserAgent;
+
+has 'config' => ( is => 'rw' , isa => 'Ref', required => 1);
+
+has 'host' => ( is => 'rw' , isa => 'Str', default => '127.0.0.1', required => 0 );
+has 'cgi_path' => ( is => 'rw', isa => 'Str', default => '/nagios/cgi-bin', required => 0 );
+has 'nagios_path' => ( is => 'rw', isa => 'Str', default => '/nagios', required => 0);
+has 'pnp4nagios_path' => ( is => 'rw', isa => 'Str', default => '/pnp4nagios',required => 0 );
+has 'user' => ( is => 'rw', isa => 'Str', default => 'nagplot', required => 0 );
+has 'pass' => ( is => 'rw', isa => 'Str', default => 'nagplot', required => 0 );
+has 'secure' => ( is => 'rw', isa => 'Str', default => 1, required => 0);
+
+sub BUILD {
+  my $self = shift;
+  my %params  = %{$self->config->{Nagios}};
+  $self->host($params{host})                       unless not defined $params{host};
+  $self->cgi_path($params{cgi_path})               unless not defined $params{cgi_path};
+  $self->nagios_path($params{nagios_path})         unless not defined $params{nagios_path};
+  $self->pnp4nagios_path($params{pnp4nagios_path}) unless not defined $params{pnp4nagios_path};
+  $self->user($params{user})                       unless not defined $params{user};
+  $self->pass($params{pass})                       unless not defined $params{pass};
+  $self->secure($params{secure})                   unless not defined $params{secure};
+}
+
+sub hosts { 
+  my $self = shift;
+  my $ua = Mojo::UserAgent->new;
+  my $url = $self->build_url();
+  $url .= "/config.cgi?type=hosts";
+  my $div = $ua->get($url)->res->dom->at('.data');
+  my $content = $div->to_xml();
+  my $te = HTML::TableExtract->new();
+  $te->parse($content);
+  my @hosts;
+  foreach my $ts ( $te->tables ) {
+    foreach my $row ( $ts->rows ) {
+      push @hosts,$row->[1];
+    }
+  }
+  shift @hosts;
+  return @hosts;
+}
+
+sub services {
+  my $self = shift;
+}
+
+sub query_state {
+  my $self = shift;
+}
+
+sub build_url{
+  my $self = shift;
+  my $url;
+  $url .= "https://" if $self->secure;
+  $url .= "http://" if not $self->secure;
+  $url .= $self->user.":".$self->pass."@";
+  $url .= $self->host;
+  $url .= "/".$self->cgi_path;
+  return $url;
+}
+
+no Moose;
+1;
