d7ee6e7ad7b7443eaefbb84abc35d92bd60513d0
Andreas Marschke <andreas.marschke@gmail.com>
2012-04-08 08:03:56 +0200

    lib/Nagplot/DataSource.pm: fix errornous way of calling DataSources

--- lib/Nagplot/DataSource.pm
+++ lib/Nagplot/DataSource.pm
@@ -9,7 +9,8 @@ Nagplot::DataSource - The source of all monitorable data
  use Nagplot::DataSource;
  my $datasource = Nagplot::DataSource->new( 
    config => {
-     DataSource => {
+     DataSources => {
+       enabled => [Nagios, Dummy],
        Nagios => {
   	 host => '127.0.0.1',
 	 cgi_path => '/nagios/cgi-bin',
@@ -38,8 +39,33 @@ Nagplot::DataSource - The source of all monitorable data
 
 =head1 DESCRIPTION
 
-Nagplot Datasource is the root of all data you can pull out of a pluggable backend.
-See Nagplot/DataSource/Backends.pod for more Info
+Nagplot Datasource is the root to all data you can pull out of a pluggable backend.
+See Nagplot::Source DEVELOPER DOCUMENTATION below for more Info. Nagplot::Source is
+the namespace for datasources.
+
+=head1 CONFIGURATION
+
+Nagplot::DataSource is configured through the DataSource key in the nagplot.conf file. It
+holds the configuration for datasources and which datasources should be used by 
+Nagplot::DataSource.
+
+This segment will concentrate on the options directly available in the DtasSource hash.
+
+=head2 enabled
+
+An array of datasources you want to have enabled. This option will make sure that only
+the relevant datasources to your setup will be enabled and running upon request, which reduces
+resource hungriness if more than your requested sources are present.
+
+=head2 cache
+
+A string describing the caching mechanism used. If none is set Nagplot::Cache::Backend::CSV is
+used.
+
+=head1 CACHING
+
+Nagplot::DataSource is capable of caching requests to the servers that are not relevant for 
+live data such as hosts and services.
 
 =cut
 
@@ -52,58 +78,106 @@ use 5.010_000;
 use strict;
 use warnings;
 use Moose;
-use Module::Pluggable search_path => 'Nagplot::DataSource', require => 1, instantiate => 'new', except => ['Nagplot::DataSource::Dummy'];
-#use Module::Pluggable search_path => 'Nagplot::DataSource', require => 1, instantiate => 'new', only => ['Nagplot::DataSource::Dummy'];
+use Module::Pluggable::Object;
+use Nagplot::DataSource::Cache;
+use Data::Dumper;
 
 has 'config' => ( is => 'rw' , isa => 'Ref', required => 1);
+has 'datasources' => ( is => 'rw' , isa => 'ArrayRef[Str]');
+has 'finder' => (is => 'rw' , isa => 'Object');
+has 'cache' => ( is => 'rw' , isa => 'Object');
+
+# setup plugins and finder
+sub BUILD {
+  my $self = shift;
+
+  $self->datasources($self->config->{enabled});
+  $self->cache(Nagplot::DataSource::Cache->new());
+}
+
+=head1 Nagplot::Source DEVELOPER DOCUMENTATION
+
+This segment will focus on functions Nagpot::Source's should implement.
+
+=head2 hosts()
+
+Takes no parameters and returns an array of hostnames as strings.
+
+=cut
 
 sub hosts {
   my $self = shift;
-
   my @hosts;
-  foreach my $plugin ($self->plugins(config => $self->config)) {
-     push (@hosts,$plugin->hosts());
+
+  # get the list of hosts from the sources and add them to the cache
+  # but only if we know the datasource is available to us
+  foreach my $source (@{$self->datasources}) {
+    my $finder = Module::Pluggable::Object->new(search_path => 'Nagplot::DataSources',
+						instantiate => 'new',
+						only => $source);
+    foreach my $datasource ($finder->plugins(config => $self->config)) {
+      push @hosts,$datasource->hosts();
+    }
   }
   return @hosts;
 }
 
+=head2 services($host)
+
+Takes the parameter $host and returns an array of services as strings. Return an empty
+array if no services are available.
+
+=cut
+
 sub services {
   my $self = shift;
   my $host = shift;
-
   my @services;
-  foreach my $plugin ($self->plugins(config => $self->config)) {
-    push (@services,$plugin->services($host));
+
+  if ($self->cache->where_is('service',$host)) {
+    return keys $self->cache->services();
+  }
+
+  # get the list of hosts from the sources and add them to the cache
+  # but only if we know the datasource is available to us
+  foreach my $source (@{$self->datasources}) {
+    $self->finder->{only} = $source;
+    foreach my $datasource ($self->finder->plugins) {
+
+      my @service_by_source = $datasource->services($host);
+      push (@services,@service_by_source);
+
+      foreach (@service_by_source) {
+	$self->cache->add_service($host,$_,$datasource);
+      }
+    }
   }
   return @services;
 }
 
-# this is very bad. (performance-wise)
-# later on maybe implement a cache 
-# that holds in which plugin $service
-# and $host are and then just use that
-# plugin to query for it.
+=head2 checkstate($host, $service)
 
-sub query_state {
+Takes the parameters $host and $service where $service is the name of the service provided
+by $host and returns it's value as a scalar value. Return 0 if no value is possible 
+to retrieve. 
+
+=cut
+
+sub checkstate {
   my $self = shift;
   my $host = shift;
   my $service = shift;
   my $state;
-  foreach my $plugin ($self->plugins(config => $self->config)) {
-    foreach my $plugin_host ($plugin->hosts) {
-      if ($plugin_host eq $host) {
-	foreach my $plugin_service ($plugin->services($host)) {
-	  if ($plugin_service eq $service) {
-	    $state = $plugin->query_state($host, $service);
-	  }
-	}
-      }
+
+  foreach my $source (@{$self->datasources}) {
+    my $datasource = $self->finder->{only} = $source;
+    foreach my $datasource ($self->finder->plugins) {
+      $datasource->checkstate($host, $service);
     }
   }
+
   return $state;
 }
 
 no Moose;
 1;
-
-
