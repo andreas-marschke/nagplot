65387de946c7d5a0a8b4df0c9cfd65259656d6ce
Andreas Marschke <andreas.marschke@gmail.com>
2012-09-20 20:54:25 +0200

    use require.js and concat it with jquery to get it working. also concat backbone with underscore to fix require() call

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+/* Author: Andreas Marschke */
+
+require(["js/vendor/base64.js",
+	 "js/vendor/backbone-underscore.js",
+	 "js/vendor/d3.min.js",
+	 "js/vendor/rickshaw.js"
+	],
+	function(){
+	    
+	});
